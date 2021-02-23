219d2b0208ac2eda8eda9dc6a80ac941d187fabc
cdanielss <cdanielss1973@outlook.com>
2021-02-23 15:20:59 -0300

    Next.js

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return (
+        <div>
+            <img src="https://github.com/cdanielss.png" alt="Imagem de Perfil"/>
+            <div>
+                <strong>Carlos Daniel</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
